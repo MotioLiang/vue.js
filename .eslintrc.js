1838249bdb78e20e98dfef6dbc9c5630c26fdbd3
MotioLiang <1398814154@qq.com>
2020-05-23 13:59:13 +0800

    'eslit'

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,42 +11,60 @@ module.exports = {
         parser: 'babel-eslint'
     },
     globals: {
-        'document': true,
-        'localStorage': true,
-        'sessionStorage': true
+        document: true,
+        localStorage: true,
+        sessionStorage: true
     },
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         // js单引号
-        quotes: ['warn', 'single'],
+        quotes: [
+            'warn',
+            'single'
+        ],
         // 缩进风格
-        indent: ['error', 4, {
-            'SwitchCase': 1
-        }],
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1
+            }
+        ],
         //  结尾不要分号
-        semi: ['error', 'never'],
+        semi: [
+            'error',
+            'never'
+        ],
         // 禁用 eval()
-        'no-eval': 2,
-        'vue/attribute-hyphenation': 'warn',
-        'vue/html-closing-bracket-newline': 'warn',
-        'vue/html-closing-bracket-spacing': 'warn',
-        'vue/html-end-tags': 'warn',
-        'vue/html-indent': ['warn', 4],
-        'vue/html-quotes': 'warn',
-        'vue/html-self-closing': 'warn',
-        'vue/max-attributes-per-line': 'warn',
+        'no-eval': 'error',
+        'vue/attribute-hyphenation': 'error',
+        'vue/html-closing-bracket-newline': 'error',
+        'vue/html-closing-bracket-spacing': 'error',
+        'vue/html-end-tags': 'error',
+        'vue/html-indent': [
+            'warn',
+            4
+        ],
+        'vue/html-quotes': 'error',
+        'vue/html-self-closing': 'error',
+        'vue/max-attributes-per-line': 'error',
         'vue/multiline-html-element-content-newline': 'warn',
         'vue/mustache-interpolation-spacing': 'warn',
         'vue/name-property-casing': 'warn',
         'vue/no-multi-spaces': 'warn',
-        'vue/no-spaces-around-equal-signs-in-attribute': 'warn',
-        'vue/no-template-shadow': 'warn',
-        'vue/prop-name-casing': 'warn',
-        'vue/require-default-prop': 'error',
-        'vue/require-prop-types': 'warn',
-        'vue/singleline-html-element-content-newline': 'warn',
-        'vue/v-bind-style': 'warn',
-        'vue/v-on-style': 'warn'
+        'vue/no-spaces-around-equal-signs-in-attribute': 'error',
+        'vue/no-template-shadow': 'error',
+        'vue/prop-name-casing': 'error',
+        'vue/require-default-prop': 'warn',
+        'vue/require-prop-types': 'error',
+        'vue/singleline-html-element-content-newline': 'error',
+        'vue/v-bind-style': 'error',
+        'vue/v-on-style': 'error',
+        'vue/attributes-order': 'warn',
+        'vue/no-confusing-v-for-v-if': 'warn',
+        'vue/no-v-html': 'warn',
+        'vue/order-in-components': 'warn',
+        'vue/this-in-template': 'error'
     }
 }
